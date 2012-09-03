a6998eef8974657eb74fc701a0328b31ded75ec5
mvanallen <michael.horl@gmail.com>
2012-09-03 19:15:45 +0200

    Initial check-in
    - added background (…I'll include a link to the original DeviantArt
    piece as soon as I can find it again..)
    - lots of restyling
    - swapped example links
    - some bug fixing

--- js/script.js
+++ js/script.js
@@ -45,16 +45,16 @@ function updateClock() {
 	currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
 	// Choose either "AM" or "PM" as appropriate
-	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+	//var timeOfDay = (currentHours < 12) ? "AM" : "PM";
 
 	// Convert the hours component to 12-hour format if needed
-	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	//currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
 
 	// Convert an hours component of "0" to "12"
-	currentHours = (currentHours == 0) ? 12 : currentHours;
+	//currentHours = (currentHours == 0) ? 12 : currentHours;
 
 	// Compose the string for display
-	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " ";// + timeOfDay;
 
 	// Fill '#clock' div with time
 	$("#clock").html(currentTimeString);
@@ -93,10 +93,17 @@ $(document).ready(function() {
 		/*  If it doesn't contain "://",  *\
 		|*  it's not a URL                *|
 		\*================================*/
-		if(/:\/\//.test(line) != true) {
+		//if(/ /:\/\//.test(line) /*|| /mailto:/.test(line)*/ ) != true) {
+		if(( /:\/\//.test(line) || /mailto:/.test(line) ) != true) {
+			/*  Split Heading and Subheading  *\
+			\*=======================*/
+			var titleArray = line.split(" : ");
+			var title = titleArray[0];
+			var subtitle = titleArray[1];
+	
 			if(count > 1)
 				html = html + '</div>';
-				html = html + '<div class="block"><h1>' + line + '</h1><ul>';
+				html = html + '<div class="block"><h1>' + title + '</h1><h2>' + subtitle + '</h2><ul>';
 				count++;
 				continue;
 		}
@@ -126,18 +133,18 @@ $(document).ready(function() {
 	
 	/*  Hide lists  *\
 	\*==============*/
-	$('ul').slideUp();
+	$('ul').hide();//.slideUp();
 
 	/*  Show on hover  *\
 	\*=================*/
 	$('.block').mouseenter(function() {
-		$('ul', this).slideDown();
+		$('ul', this).slideDown('fast');//.show('fast');
 	});
 
 	/*  Hide on unhover  *\
 	\*===================*/
 	$('.block').mouseleave(function() {
-		$('ul', this).slideUp();
+		$('ul', this).slideUp('fast');//.hide('fast');
 	});
 
 
